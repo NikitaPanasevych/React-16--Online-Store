e9c034aa489dd15ab9e9a283b6ca9d4ce0fb422d
DeStRaaM <20destraam03@gmail.com>
2022-11-03 22:46:24 +0100

    description page update.

--- src/Pages/PDP.jsx
+++ src/Pages/PDP.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./PDP.css";
 import parse from 'html-react-parser';
+import GalleryElement from "../Components/PDP/galleryElement";
 
 export default class PDP extends React.Component{
     constructor(props){
@@ -15,20 +16,42 @@ export default class PDP extends React.Component{
         return{data: props.data}
       }
 
+      changeImage = (clickedImageId) => {
+        this.setState({selectedPhoto: clickedImageId})
+      }
+
     render(){
         return(
             <>
-            {this.state.data?  
+            {this.state.data ?  
             <div className=" ItemDescriptionPage">
                     <div className=" Gallery">
-                        {this.props.data[0].gallery.map(element=><img className=" GalleryElement" src={element} />)}
+                        {
+                            this.props.data[0].gallery.map((element, index) =>
+                            <GalleryElement 
+                            className=" GalleryElement" 
+                            src={element}
+                            id={index} 
+                            changeImage={this.changeImage} 
+                            />
+                        )}
                     </div>
                     <div >
-                        <img className=" ProductImage" src={this.props.data[0].gallery[0]}/>
+                        <img className=" ProductImage" src={this.props.data[0].gallery[this.state.selectedPhoto]}/>
                     </div>
                     <div className=" ProductDescr">
                         <h1 className="Brand">{this.state.data[0].brand}</h1>
                         <p className=" Name">{this.state.data[0].name}</p>
+                        {this.state.data[0].attributes.map(element=><p>
+                        {element.id + ":"}  
+                        <bt />
+                        {this.state.data[0].attributes[0].items.map(prod=>prod.id)} 
+                        </p>)}
+                        <p className="ProductPrice">
+                        {this.state.data[0].prices[0].__typename + ":"}
+                        <br />
+                        {this.state.data[0].prices[0].currency.symbol + this.state.data[0].prices[0].amount}
+                        </p>
                         <button className="AddToCartBtn">ADD TO CART</button>
                         {parse(String(this.state.data[0].description))}
                     </div>
